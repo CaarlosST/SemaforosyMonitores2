a2eda9205d4bcb7926f2c809cd6ad0e3fff9994d
CaarlosST <153277622+CaarlosST@users.noreply.github.com>
2023-12-15 12:52:40 +0100

    Update Puente.java

--- Puente.java
+++ Puente.java
@@ -3,32 +3,30 @@ import java.util.Queue;
 import java.util.Random;
 
 public class Puente {
-	private boolean libre = true; // El puente comienza libre
+	private boolean libre = true; // El puente esta  libre
 	private Queue<Integer> colaPuente = new LinkedList<>();
 
 	public synchronized void cruzar(String direccionCoche) throws InterruptedException {
-		while (!libre) {
-			wait(); // Espera si el puente está ocupado
+		while (!libre) { //libre = false 
+			wait(); 
 		}
 		
 		Random random = new Random();
 		int numCoches = (random.nextInt(15) + 1);
 		System.out.println("Coches entrando desde el " + direccionCoche + ": " + numCoches+". ");
 		for (int i = 0; i < numCoches; i++) {
-			colaPuente.add(random.nextInt(1000)); // Simulación de coches cruzando
+			colaPuente.add(random.nextInt(1000)); 
 			System.out.println("Coche id : " + random.nextInt(100) + " 	posicion -> " + (i + 1));
 		}
-		libre = false;
-		// El puente está ocupado
-		notify(); // Notifica a otros coches que están esperando
+		libre = false; // El puente está ocupado		
+		notify(); // Notifica a otros coches que el esta ocupado
 	}
 
 	public synchronized void saliendo() {
 		
 		
-		colaPuente.clear();
-		libre = true; // Libera el puente
-		// Borra la cola de coches que cruzaron
+		colaPuente.clear(); 
+		libre = true; // El puente esta libre 		
 		notify(); // Notifica a otros coches que el puente está libre
 		System.out.println("Todos los coches han salido del puente.");
 	}
@@ -60,7 +58,7 @@ class Coches extends Thread {
 				puente.cruzar(direccionCoche);
 				sleep(1000); // Simula el tiempo que lleva cruzar el puente
 				puente.saliendo();
-				sleep(1000);
+				sleep(1000);  // Simula el tiempo de espera 
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
